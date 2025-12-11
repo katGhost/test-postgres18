de6545efc2c19e4e36bd202604af1cc62415f66b
katGhost <katghost124@gmail.com>
2025-12-11 14:19:16 +0200

    Postgres db initialized

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test-Postgres18
+
+## Description
+
+Testing postgres18 with Docker. PostgreSQL 18 is said to be super fast with File I/O, leveraging the power of Asynchronous and multiple file management [Downloads and Uploads].
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -4,6 +4,31 @@ from flask import Flask, request, send_from_directory, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('project.config.Config')
+db = SQLAlchemy(app)
+
+# create absolute path for upload folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
+# make sure the directorr exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Defile a model for file metadata
+class File(db.Model):
+    __tablename__ = 'files'
+
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    file_path = db.Column(db.Text, nullable=False)
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+    def __repr__(self):
+        return f'<File {self.filename}>'
+
 
 # route for home page
 @app.route('/')
