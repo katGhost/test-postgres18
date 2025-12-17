2ed077fcb2b38fca578e14871def98ddb98de953
katGhost <katghost124@gmail.com>
2025-12-17 10:38:29 +0200

    adding redis check

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
+# check postgres readiness
 echo "Waiting for Postgres..."
 while ! nc -z db 5432; do
   sleep 0.2
 done
 echo "PostgreSQL is up"
+
+# Add redis check readiness
+echo "Waiting for Redis..."
+while ! nc -z redis 6379; do
+  sleep 0.2
+done
+echo "Redis is up"
 
 echo "Running DB setup..."
 python manage.py create_db || true
